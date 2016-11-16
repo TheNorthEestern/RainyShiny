818ed915d7d3c2dbd007b99e75c4462e2a1881a7
Kacy James <inquen@gmail.com>
2016-11-16 09:04:10 -0500

    Initial commit

---
+++ RainyShiny/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RainyShiny
+//
+//  Created by Kacy James on 11/15/16.
+//  Copyright © 2016 Student Driver. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = ""
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "&lat="
---
+++ RainyShiny/WeatherViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  RainyShiny
+//
+//  Created by Kacy James on 11/14/16.
+//  Copyright © 2016 Student Driver. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherViewCell", for: indexPath)
+        return cell
+    }
+    
+}
+
